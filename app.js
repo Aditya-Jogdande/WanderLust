2eb3cee457c7be613b0bec9c2d7fa5c5e167f54d
Aditya-Jogdande <186449088+Aditya-Jogdande@users.noreply.github.com>
2026-07-23 21:26:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -93,6 +93,10 @@ app.listen("1111", () => {
   console.log("Server is running on : 1111 port");
 });
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 app.all("/*path", (req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
 });
